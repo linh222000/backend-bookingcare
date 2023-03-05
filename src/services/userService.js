523d399e0950170764ba6e267af269a6c08ab685
Pham Linh <phamvuonglinh2000@gmail.com>
2023-03-05 23:30:58 +0700

    modified:   .env.example 	modified:   src/controllers/userController.js 	modified:   src/route/web.js 	modified:   src/server.js 	modified:   src/services/userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -25,7 +25,7 @@ let handleUserLogin = (email, password) => {
             if (isExist) {
                 //user already exist
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true,
 
@@ -206,10 +206,34 @@ let updateUserData = (data) => {
         }
     })
 }
+let getallCodeService = (typeInput) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if (!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            } else {
+                let res = {};
+                let allcode = await db.Allcode.findAll({
+                    where: { type: typeInput }
+                });
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            }
+            
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUsers: getAllUsers,
     createNewUser: createNewUser,
     deleteUser: deleteUser,
-    updateUserData: updateUserData
+    updateUserData: updateUserData,
+    getallCodeService: getallCodeService
 }
